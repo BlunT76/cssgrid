3885249756976a2361c8d61fcae70654d185af4a
xLeDocteurx <lenoirc.fr@gmail.com>
2018-05-16 16:54:13 +0200

    Continued work on the css exporter module

--- generate-css.js
+++ generate-css.js
@@ -1,13 +1,54 @@
 
 function modal(formatedCode) {
     
-    document.getElementById("generated-code").innerHTML = formatedCode;
+    document.getElementById("generated-code").innerHTML = 
+    `
+    <p>
+    
+    </p>
+    <p>
+    `/* Contenu CSS des enfants */`
+    </p>
+    `;
 }
 
-class formatedCSS {
+var containerCSS = {
 
-    constructor (x) {
+    display : "",
 
-        this.x = x;
+    // FLEX : Sur les containers
+    flex_direction : "",
+    justify_content : "",
+    flex_wrap : "",
+    align_items : "",
+    align_content : "",
+
+    flex_grow : "",
+    align_self : "",
+    order : "",
+
+    // GRID : Sur les containers
+    grid_template : "",
+    grid_template_areas : "",
+    grid_template_rows : "",
+    grid_template_columns : "",
+    grip_gap : "",
+    grid_auto_rows : "",
+
+    children : [] /* Contient des objets "cssContentCSS" */
+}
+
+class cssContentCSS {
+
+    constructor (align_self, flex_grow, order, grid_column, grid_row) {
+
+// FLEX : sur les contenus
+        this.align_self = align_self;
+        this.flex_grow = flex_grow;
+        this.order = order;
+
+// GRID : Sur les contenus
+        this.grid_column = grid_column;
+        this.grid_row = grid_row;
     }
 }
